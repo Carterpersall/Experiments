7f9f1d4735f5bde0be26dd7b9bfa746d618cf300
Carterpersall <carterpersall@gmail.com>
2022-05-10 14:08:26 -0500

    Update IP Scanner.ps1

--- PowerShell/IP Scanner.ps1
+++ PowerShell/IP Scanner.ps1
@@ -1,7 +1,6 @@
 while(!$done){
-    $ipBase = Read-Host "Enter the Beginning of the IP Range(Ex:192.168.0.)"
-    if($null -eq $ipBase){Write-Host "You must enter a value"}
-    if($ipBase.Substring($ipBase.Length - 2,$ipBase.Length - 1 -ne ".")){
+    $ipBase = Read-Host "Enter the Beginning of the IP Range(Ex:192.168.0. or 10.38.)"
+    if(($null -ne $ipBase) -and ($ipBase.Substring($ipBase.Length - 1, 1) -ne ".")){
         Write-Host "You must enter a value with a period at the end"
     }else{
         $done = $true
@@ -9,60 +8,161 @@ while(!$done){
 }
 $done = $null
 
-while(!$done){
-    $rangeB = Read-Host "Enter the Beginning of the Range of IPs to check(0 - 255)"
-    if($null -eq $rangeB){Write-Host "You must enter a value"}
-    if($rangeB -lt 0 -or $rangeB -gt 255){
-        Write-Host "Invalid Value"
-    }else{
-        $done = $true
-    }
-}
-$done = $null
+function getRange{
+    while(!$done){
+        $rangeBeg = [int](Read-Host "Enter the Beginning of the Range of IPs to check(0 - 255)")
 
-while(!$done){
-    $rangeE = Read-Host "Enter the End of the Range of IPs to check(0 - 255)"
-    if($null -eq $rangeE){Write-Host "You must enter a value"}
-    if($rangeB -lt 0 -or $rangeB -gt 255){
-        Write-Host "Invalid Value"
-    }else{
-        $done = $true
-    }
-}
-$done = $null
+        if($null -eq $rangeBeg){Write-Host "You must enter a value"}
 
-$range = $rangeB..$rangeE
+        if(($rangeBeg -lt 0) -or ($rangeBeg -gt 255)){
+            Write-Host "Invalid Value"
+        }else{
+            $done = $true
+        }
+    }
+    $done = $null
+    
+    while(!$done){
+        $rangeEnd = [int](Read-Host "Enter the End of the Range of IPs to check(0 - 255)")
+
+        if($null -eq $rangeEnd){Write-Host "You must enter a value"}
+
+        if(($rangeEnd -lt 0) -or ($rangeEnd -gt 255) -or ($rangeEnd -lt $rangeBeg)){
+            Write-Host "Invalid Value"
+        }else{
+            $done = $true
+        }
+    }
+    $done = $null
+
+    return $rangeBeg..$rangeEnd
+}
+
+Write-Host "First Range(192.168.0.X):"
+
+$rangeOne = getRange
+
+if(($ipBase.ToCharArray() | Where-Object {$_ -eq '.'} | Measure-Object).Count -lt 3){
+    Write-Host "Second Range(192.168.X.0):"
+
+    $rangeTwo = getRange
+}
+
+if(($ipBase.ToCharArray() | Where-Object {$_ -eq '.'} | Measure-Object).Count -lt 2){
+    Write-Host "Third Range(192.X.0.0):"
+
+    $rangeThree = getRange
+}
+
+if($null -eq $ipBase){
+    Write-Host "Fourth Range(X.0.0.0):"
+
+    $rangeFour = getRange
+}
 
 $printResolved = Read-Host "Print Resolved IPs?(y/n)"
-if ($printResolved -eq "y") {$printResolved = $true}else{$printResolved = $false}
+if ($printResolved -eq "y"){$printResolved = $true}else{$printResolved = $false}
 
-$range | ForEach-Object {
-    #Merge the given IP beginning with the current point in the range
-    $ip = "$ipBase$_"
-    #Create a progress bar
-    Write-Progress -Activity "Queuing IP Address Scans" -Status "Queuing IP Address: $ip" -PercentComplete ([Math]::Truncate($_ * 100/$range.Count))
-    #Queue the IP addresses to be scanned into separate threads
-    Start-Job -InputObject "$ip" -ScriptBlock {
-        Get-WmiObject Win32_PingStatus -Filter "Address='$input' and Timeout=200 and ResolveAddressNames='true' and StatusCode=0" | Select-Object ProtocolAddress*
+if($rangeFour){
+    $rangeCount = $rangeOne.Count * $rangeTwo.Count * $rangeThree.Count * $rangeFour.Count
+}elseif($rangeThree){
+    $rangeCount = $rangeOne.Count * $rangeTwo.Count * $rangeThree.Count
+}elseif($rangeTwo){
+    $rangeCount = $rangeOne.Count * $rangeTwo.Count
+}else{
+    $rangeCount = $rangeOne.Count
+}
+
+
+if ($null -ne $rangeFour){
+    foreach ($numOne in $rangeOne){
+        foreach ($numTwo in $rangeTwo){
+            foreach ($numThree in $rangeThree){
+                foreach ($numFour in $rangeFour){
+                    #Merge the given IP base with the current point in the range
+                    $ip = $numFour + "." + $numThree + "." + $numTwo + "." + $numOne
+
+                    #Create a progress bar
+                    Write-Progress -Activity "Queuing IP Address Scans" -Status "Queuing IP Address: $ip" -PercentComplete ([Math]::Truncate(($numFour * $numThree * $numTwo * $numOne) * 100/$rangeCount))
+
+                    #Queue the IP addresses to be scanned into separate threads
+                    Start-Job -InputObject "$ip" -ScriptBlock {
+                        Get-WmiObject Win32_PingStatus -Filter "Address='$input' and Timeout=200 and ResolveAddressNames='true' and StatusCode=0" | Select-Object ProtocolAddress*
+                    } | Out-Null
+                }
+            }
+        }
+    }
+}elseif($null -ne $rangeThree){
+    foreach ($numOne in $rangeOne){
+        foreach ($numTwo in $rangeTwo){
+            foreach ($numThree in $rangeThree){
+                #Merge the given IP base with the current point in the range
+                $ip = $ipBase + $numThree + "." + $numTwo + "." + $numOne
+
+                #Create a progress bar
+                Write-Progress -Activity "Queuing IP Address Scans" -Status "Queuing IP Address: $ip" -PercentComplete ([Math]::Truncate(($numOne) * 100/$rangeOne.Count))
+
+                #Queue the IP addresses to be scanned into separate threads
+                Start-Job -InputObject "$ip" -ScriptBlock {
+                    Get-WmiObject Win32_PingStatus -Filter "Address='$input' and Timeout=200 and ResolveAddressNames='true' and StatusCode=0" | Select-Object ProtocolAddress*
+                } | Out-Null
+            }
+        }
+    }
+}elseif($null -ne $rangeTwo){
+    foreach ($numOne in $rangeOne){
+        foreach ($numTwo in $rangeTwo){
+            #Merge the given IP base with the current point in the range
+            $ip = $ipBase + $numTwo + "." + $numOne
+
+            #Create a progress bar
+            Write-Progress -Activity "Queuing IP Address Scans" -Status "Queuing IP Address: $ip" -PercentComplete ([Math]::Truncate(($numOne) * 100/$rangeOne.Count))
+
+            #Queue the IP addresses to be scanned into separate threads
+            Start-Job -InputObject "$ip" -ScriptBlock {
+                Get-WmiObject Win32_PingStatus -Filter "Address='$input' and Timeout=200 and ResolveAddressNames='true' and StatusCode=0" | Select-Object ProtocolAddress*
+            } | Out-Null
+        }
+    }
+}else{
+    foreach ($numOne in $rangeOne){
+        #Merge the given IP base with the current point in the range
+        $ip = $ipBase + $numOne
+
+        #Create a progress bar
+        Write-Progress -Activity "Queuing IP Address Scans" -Status "Queuing IP Address: $ip" -PercentComplete ([Math]::Truncate(($numOne) * 100/$rangeOne.Count))
+
+        #Queue the IP addresses to be scanned into separate threads
+        Start-Job -InputObject "$ip" -ScriptBlock {
+            Get-WmiObject Win32_PingStatus -Filter "Address='$input' and Timeout=200 and ResolveAddressNames='true' and StatusCode=0" | Select-Object ProtocolAddress*
+        } | Out-Null
     }
 }
+
 
 do{
     $num = 0
     Get-Job | ForEach-Object{
         if($_.State -eq 'Completed'){$num++}
     }
-    Write-Progress -Activity "IP Address Scan Progress" -Status "IP Addresses Scanned: $num" -PercentComplete ([Math]::Truncate($num * 100/$range.Count))
-    Start-Sleep -s 1
-}until($num -eq $range.Count)
 
+    #Show Scan Completion Progress
+    Write-Progress -Activity "IP Address Scan Progress" -Status "IP Addresses Scanned: $num" -PercentComplete ([Math]::Truncate(($numOne) * 100/$rangeOne.Count))
+
+    Start-Sleep -s 1
+}until($num -eq $rangeCount) #Won't stop until all scans are complete
+
+
+#Output the results
 Write-Host "Found Addresses:"
-if ($printResolved) {
+if ($printResolved){
     Get-Job | Receive-Job | Select-Object ProtocolAddress*
 }else{
-    $temp = Get-Job | Receive-Job | Select-Object ProtocolAddress | Out-String
-    $temp = $temp -replace "ProtocolAddress" -replace "-"
-    $temp.Substring(6,$temp.Length-17)
+    $output = Get-Job | Receive-Job | Select-Object ProtocolAddress | Out-String
+    $output = $output -replace "ProtocolAddress" -replace "-"
+    $output
 }
 
+#Remove Completed Jobs
 Get-Job | Remove-Job
